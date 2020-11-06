6d48e39c761c11f7e8555b2d93c9c48fbaa80c82
ncros <ncros@macbook-pro-de-ncros.home>
2020-11-06 14:51:50 +0100

    ajout d'un widget textview, appel depuis java

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        var text = TextView(this)
+        text.text = "Bonjour, vous me devez 1 000 000 euros."
+        setContentView(text)
     }
 }
